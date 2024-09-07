5783943ed632c16b1fd9af6a375503d5b6e4ec99
rana <md.karim@summitcommunications.net>
2024-09-07 11:34:29 +0600

    sign in

--- realTimeChattingApp/src/components/registration/index.jsx
+++ realTimeChattingApp/src/components/registration/index.jsx
@@ -6,22 +6,27 @@ const RegistrationFormComponent = () => {
       <div>
         <form>
           <div>
-            <label className="font-fontInter">Enter Email</label>
+            <label className="font-fontInter text-[#484848]">Enter Email</label>
             <input className="w-full px-3 py-2 border rounded-md outline-none mb-3 mt-2" />
           </div>
           <div>
-            <label className="font-fontInter">Enter Password</label>
+            <label className="font-fontInter text-[#484848]">
+              Enter Password
+            </label>
             <input className="w-full px-3 py-2 border rounded-md outline-none mb-3 mt-2" />
           </div>
           <div>
-            <label className="font-fontInter">Enter Confirm Password</label>
+            <label className="font-fontInter text-[#484848]">
+              Enter Confirm Password
+            </label>
             <input className="w-full px-3 py-2 border rounded-md outline-none mb-3 mt-2" />
           </div>
           <button className="bg-black text-white w-full font-fontInter text-base border rounded-md py-2 mt-1 mb-4">
             Sign Up
           </button>
-          <p>
-            Already have an account please <span>sign In</span>{" "}
+          <p className="font-fontInter text-base">
+            Already have an account please{" "}
+            <span className="text-[#236DB0]">sign in</span>
           </p>
         </form>
       </div>
